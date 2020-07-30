47fc76cb8e7ff128a45d6804fb156d11bf2c3676
TakingFire <bacurrah@gmail.com>
2020-07-30 01:29:04 -0700

    Minor tweaks
    Check for discrepancies after pull!

--- readme.md
+++ readme.md
@@ -5,14 +5,11 @@
 - cuDNN v6.0 for Cuda 8.0
 - Tensorflow version 1.4
 
-You may need a special python installation I have, though it's too big to upload here.\
-Make sure the custom python distro is added to %path%.
+**TO RUN:**\
+To make this run properly, unzip the PythonDistro file into a folder on your computer seperate from the repository, inside you should find runner.py.\
+Add the folder that runner.py is located in to PATH, then in the console type "runner start.py" from the directory containing the repository and the start.py program.
 
 The file structure should be self repairing if you break it.
 
 Add your own datasets to data/DATASETNAME/YOUR IMAGES.\
 Select the dataset by typing the DATASETNAME folder title in the UI.
-
-**TO RUN:**\
-To make this run properly, unzip the PythonDistro file into a folder on your computer seperate from the repository, inside you should find runner.py.\
-Add the folder that runner.py is located in to PATH, then in the console type "runner start.py" from the directory containing the repository and the start.py program.
--- start.py
+++ start.py
@@ -4,9 +4,9 @@ import sys
 import PIL
 from PIL import Image
 from getdata import getdata
-import time
+from time import sleep
 import pyautogui as gui
-import datetime
+from datetime import datetime
 import scipy.misc
 import numpy as np
 from model import DCGAN
@@ -27,9 +27,9 @@ def start_train_thread():                                       #start network t
 
 def time_thread():                              #thread to keep track of time
     print("Time:Time thread started")
-    while True:
-        window.FindElement('_time_').Update(getTime())
-        time.sleep(1)
+    # while True:
+    #     window.FindElement('_time_').Update(getTime())
+    #     time.sleep(1)
 
 def start_time_thread():                                    #starts the time thread
     threading.Thread(target=time_thread,daemon=True).start()
@@ -39,7 +39,7 @@ def start_time_thread():                                    #starts the time thr
 #utility function definitions
 ###########################################################################################
 def getTime():                                              #function to get the current time and format it
-    return datetime.datetime.now().strftime('%H:%M:%S')
+    return datetime.now().strftime('%H:%M:%S')
 
 def verify_image(img_file):             #Checks wheather or not an image file is valid
      try:
@@ -52,7 +52,7 @@ def verify_image(img_file):             #Checks wheather or not an image file is
         return True                     #returns the state of the image file
 
 def resize1():                          #resizes all the images in the complete directory
-    print("Resizeing")
+    print("Resizing")
     while True:
         for image in os.listdir("complete/"):
             basewidth = 1000
@@ -138,24 +138,40 @@ def main(_):                                                #main network initia
 
 #UI function
 ###########################################################################################
-layout = [                                                                          #UI Layout
-          [sg.Text("key"),sg.Input(),sg.Image(filename="./icons/logo.png")],
-          [sg.Text("Epoches"),sg.Slider(range=(1,500),orientation='h'),sg.Text('   Time: '),sg.Text('', key='_time_',size=(20,1))],
-          [sg.Text('Batches '),sg.Slider(range=(1,256),orientation='h'),sg.Checkbox(text="USE GPU",default=True)],
-          [sg.Button("Start"),sg.Text('LOG')],
-          [sg.Output(size=(65,5))],
-          [sg.Button("EXIT")]
-          ]
-window = sg.Window("LILIAN", layout)        #initialize the window
+
+ui_back, ui_mid, ui_front, ui_text = '#373737', '#404040', '#505050', '#A4A4A4'
+sg.theme_background_color(ui_mid)
+sg.theme_button_color((ui_text, ui_back))
+sg.theme_element_background_color(ui_mid)
+sg.theme_input_background_color(ui_back)
+sg.theme_text_element_background_color(ui_mid)      # Set up custom theme
+sg.theme_element_text_color('white')
+sg.theme_input_text_color('white')
+sg.theme_text_color(ui_text)
+sg.theme_element_text_color(ui_text)
+sg.theme_input_text_color(ui_text)
+# sg.theme('SystemDefaultForReal')
+
+frame1 = [[sg.Text('Key:         '), sg.Input(size=(12,1))],
+          [sg.Text('Epochs:     '), sg.Spin([i for i in range(1, 501)], 5, size=(10,1))],
+          [sg.Text('Batch Size:'), sg.Spin([i for i in range(1, 65)], 1, size=(10,1))],
+          [sg.Radio('CPU', 1), sg.Radio('GPU', 1, True)]]
+
+frame2 = [[sg.ProgressBar(100, size=(52,8), bar_color=(ui_front, ui_back), key='prog')],
+          [sg.Output((80,7))]]
+
+layout = [[sg.Column([[sg.Frame('Setup', frame1)],
+                      [sg.Button('Start', size=(8,1)),
+                       sg.Button('Stop', size=(6,1), disabled=True),
+                       sg.Button('Exit', size=(4,1))]]),
+           sg.Frame('Status', frame2)]]
+
+window = sg.Window('LILLIAN', layout, icon='./icons/logo-flat2.ico') #logo-flat1 for light icon
 
 timeinitialized = False
 
-while True:                                 #UI function
-    event, value = window.read()
-    if timeinitialized == False:        #check if the time thread has been started, if not start it
-        start_time_thread()
-        timeinitialized = True
-
+while True:
+    event, value = window.read()            #initialize the window
 
     if event == sg.WIN_CLOSED:              #exit program if the window is closed
         exit()
@@ -172,8 +188,8 @@ while True:                                 #UI function
         epoch = int(value[2])
         batchsz = int(value[3])
 
-        if value[4] == False:                           #disable the GPU device if unchecked in UI
-            os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+        if value[4] == False:
+            os.environ["CUDA_VISIBLE_DEVICES"] = "-1"        #disable the GPU device if unchecked in UI
             print("Manager:USING CPU")
 
         print("Manager:USING GPU")
@@ -189,7 +205,9 @@ while True:                                 #UI function
         xm = xm
         ym = ym
 
-        print("Manager:Current time is " +str(datetime.datetime.now()))                         #get time and display in debug window
+        print("Manager:Current time is " +str(datetime.now()))      #get time and display in debug window
+        window['Start'].Update(disabled=True)
+        window['Stop'].Update(disabled=False)                                # Update Buttons
 
 
         flags = tf.app.flags                                                                    #set network flags
